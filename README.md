4983197582f401433578cfd16acb56b9e6ccb18c
B1nj0y <idegorepl@gmail.com>
2018-01-24 15:48:54 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ngx_mruby versions list
+
+A script in Ruby to get [ngx_mruby](https://github.com/matsumotory/ngx_mruby) versions from GitHub releases API. Mainly used for the [asdf](https://github.com/asdf-vm/asdf).
+
+Run:
+
+```bash
+ruby get_versions.rb
+```
+
+Available `ngx_mruby` versions'll be written into the file `versions.txt`.
---
+++ get_versions.rb
@@ -0,0 +1,12 @@
+require 'rest-client'
+require 'json'
+
+def get_versions()
+  api_url = 'https://api.github.com/repos/matsumotory/ngx_mruby/releases'
+  JSON.parse(RestClient.get api_url || []).sort_by {|r| r["created_at"]}.map do |r|
+    r["tag_name"].sub(/^v/, "") || ""
+  end
+end
+
+vers = get_versions
+File.write("./versions.txt", vers.join("\n")) if vers.length > 0
