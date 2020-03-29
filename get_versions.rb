a2f0f9c11990e076fb7ef61b55374539b6dc60af
B1nj0y <idegorepl@gmail.com>
2020-03-29 19:46:26 +0800

    Update get_versions

--- get_versions.rb
+++ get_versions.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 require 'json'
 
 def get_versions()
-  api_url = 'https://api.github.com/repos/matsumotory/ngx_mruby/releases'
+  api_url = 'https://api.github.com/repos/matsumotory/ngx_mruby/releases?page=1&per_page=50'
   JSON.parse(RestClient.get api_url || []).sort_by {|r| r["created_at"]}.map do |r|
     r["tag_name"].sub(/^v/, "") || ""
   end
